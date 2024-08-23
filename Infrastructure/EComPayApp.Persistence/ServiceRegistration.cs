4ae61eb01c3f721ef2306f6cdf1d9158ddd5fac8
elmin007 <imanverdiyevemin414@gmail.com>
2024-08-23 21:43:06 +0400

    Added some chnages on the project

---
+++ Infrastructure/EComPayApp.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using EComPayApp.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComPayApp.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<EComPayAppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.ConnectionString);
+            });
+        }
+    }
+
+}
