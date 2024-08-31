17aa69119c70000f3a558101cb5fcca05ae871c9
elmin007 <imanverdiyevemin414@gmail.com>
2024-08-31 22:28:53 +0400

    Added some part of validators

---
+++ Core/EComPayApp.Application/DTOs/AboutDtos/CreateAboutDto.cs
@@ -0,0 +1,12 @@
+﻿using EComPayApp.Application.Interfaces.DTO;
+
+namespace EComPayApp.Application.DTOs.AboutDtos
+{
+    public class CreateAboutDto : IDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Vision { get; set; }
+        public string Mission { get; set; }
+    }
+}
